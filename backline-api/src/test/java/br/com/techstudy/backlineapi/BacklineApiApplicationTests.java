a7abe157aaa22b2cbee943b633dcaf84a395b42b
Flavio Medeiros <flaviotibra@gmail.com>
2022-05-08 16:13:04 -0300

    first commit

---
+++ backline-api/src/test/java/br/com/techstudy/backlineapi/BacklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techstudy.backlineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
